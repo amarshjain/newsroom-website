3341342297bde0d4d449ead05418077b77c8130e
mmaj5524 <58552714+mmaj5524@users.noreply.github.com>
2020-01-08 01:23:17 +0530

    Add files via upload

--- NEWSROOM/cml.js
+++ NEWSROOM/cml.js
@@ -44,6 +44,62 @@ const setupUI = (user) => {
                         clabpost = document.getElementById("artfpost");
                         clabpost.style.display = 'block';
                     }
+                    if(snapshot.val().ecel){
+                        clabpost = document.getElementById("ecelpost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().lci){
+                        clabpost = document.getElementById("lcipost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().foto){
+                        clabpost = document.getElementById("fotopost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().litm){
+                        clabpost = document.getElementById("litmpost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().wtc){
+                        clabpost = document.getElementById("wtcpost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().mant){
+                        clabpost = document.getElementById("mantpost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().arka){
+                        clabpost = document.getElementById("arkapost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().ffi){
+                        clabpost = document.getElementById("ffipost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().kart){
+                        clabpost = document.getElementById("kartpost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().udan){
+                        clabpost = document.getElementById("udanpost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().acm){
+                        clabpost = document.getElementById("acmpost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().fipi){
+                        clabpost = document.getElementById("fipipost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().sese){
+                        clabpost = document.getElementById("sesepost");
+                        clabpost.style.display = 'block';
+                    }
+                    if(snapshot.val().siam){
+                        clabpost = document.getElementById("siampost");
+                        clabpost.style.display = 'block';
+                    }
             firebase.database().ref('-messages/' + currentUser.uid).once('value').then(function(snapshot) {
                 $("#asvalidationCustom01").val(snapshot.val().fname);
                 $("#asvalidationCustom02").val(snapshot.val().lname);
